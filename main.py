424ec1b1bd146981c791c74c15b70e775d9a9fdc
Claudimar Bezerra <claudimards@users.noreply.github.com>
2020-05-01 10:44:53 -0300

    Adicionando função que retorna filme da API

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from flask import Flask, request
+import json
+import requests
+
 app = Flask(__name__)
+
+api_token = 'c158b2f342a0f004be2bf12d2a039b42'
+headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer{0}'.format(api_token)}
+
+#Solução encontada para mostrar busca da API
+@app.route('/filmes')
+def filmes():
+    api_url = 'https://api.themoviedb.org/3/search/movie?api_key=c158b2f342a0f004be2bf12d2a039b42&language=en-US&query=avatar&page=1&include_adult=false'
+    response = requests.get(api_url, headers)
+    if response.status_code == 200:
+        return json.loads(response.content.decode('UTF-8'))
+    else:
+        return None
+# final da função
 
 @app.route('/')
 def retorna_ok():
