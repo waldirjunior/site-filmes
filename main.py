865fc4af90cd8013467587c45ebd8d8c4880c7da
Waldir Mauricio de Miranda Junior <CIANDT\wjunior@lnb024609cps.ciandt.global>
2020-05-02 23:57:50 -0300

    Alteração como sugestão

--- main.py
+++ main.py
@@ -21,14 +21,6 @@ def filmes(nome_do_filme):
         return {}
 # final da função
 
-@app.route('/')
-def retorna_ok():
-    return 'OK!'
-
-@app.route('/site_de_filmes')
-def site_de_filmes():
-    filme = request.args.get('filme')
-    return '<h1>O filme procurado foi: {}</h1>'.format(filme)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
